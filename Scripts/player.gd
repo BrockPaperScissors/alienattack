ac7f6f8f7ff25dcf546d462d1be59aae5b8e0ce1
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-07-23 09:31:45 -0700

    fixed rocket transform/position issue

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,8 @@ var speed = 500
 
 var rocket_scene = preload("res://Scenes/rocket.tscn")
 
+@onready var rocket_container = $RocketContainer #short method alternative to get_node("RocketContainer")
+
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -27,6 +29,7 @@ func _physics_process(delta):
 	
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 75
 	
