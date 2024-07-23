cbeadc59b4a17ffab458d1a1ad1bf6a840d92f4f
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-07-23 11:47:45 -0700

    added randomness to enemy spawn position

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
 @onready var timer = $Timer
+@onready var enemy_container = $EnemyContainer
+@onready var spawn_positions = $SpawnPositions
+
+var enemy_scene = preload("res://Scenes/enemy.tscn")
 
 func _ready():
 	timer.connect("timeout", _on_timeout)
 	
 func _on_timeout():
 	print("times up")
+	SpawnEnemy()
+
+func SpawnEnemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	
+	var enemy_instance = enemy_scene.instantiate()
+	enemy_container.add_child(enemy_instance)
+	enemy_instance.global_position = random_spawn_position.global_position
+	
+	
