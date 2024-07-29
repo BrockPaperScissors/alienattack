8735b40a4ff7f570d053246b751e5755da1a13a9
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-07-28 23:23:06 -0700

    Added lives to the HUD scene and into the player UI

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,6 +1,11 @@
 extends Control
 
 @onready var score = $Score
+@onready var lives = $Lives
 
 func set_score_label(new_score):
 	score.text = "SCORE: " + str(new_score)
+
+func set_lives_label(player_lives):
+	lives.text = "LIVES: " + str(player_lives)
+	
